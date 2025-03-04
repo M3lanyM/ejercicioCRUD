bbb3dfdaad316ab624a633044b4e00d557508fea
Melany Mendoza <melanymendoza2001@gmail.com>
2025-03-04 13:56:55 -0600

    updatePDF

--- EjercicioCRUD/Controllers/ItemController.cs
+++ EjercicioCRUD/Controllers/ItemController.cs
@@ -75,40 +75,39 @@ namespace EjercicioCRUD.Controllers
             Document doc = new Document(PageSize.A4, 20, 20, 30, 50);
             MemoryStream memoryStream = new MemoryStream();
             PdfWriter writer = PdfWriter.GetInstance(doc, memoryStream);
-            writer.PageEvent = new PDFNumPag(); 
+            writer.PageEvent = new PDFNumPag();
 
             doc.Open();
 
-            
             Paragraph encabezado = new Paragraph("Reporte de Ítems", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16));
             encabezado.Alignment = Element.ALIGN_CENTER;
             doc.Add(encabezado);
             doc.Add(new Paragraph($"Generado el: {DateTime.Now:dd/MM/yyyy HH:mm}\n\n"));
 
-            
-            PdfPTable tabla = new PdfPTable(5);
+            PdfPTable tabla = new PdfPTable(8);
             tabla.WidthPercentage = 100;
-            float[] columnWidths = { 10f, 30f, 20f, 20f, 20f };
+            float[] columnWidths = { 10f, 20f, 25f, 15f, 10f, 10f, 15f, 15f };
             tabla.SetWidths(columnWidths);
 
-            
-            string[] headers = { "ID", "Código", "Descripción", "Precio", "Cantidad" };
+            string[] headers = { "ID", "Código", "Descripción", "Precio", "Cantidad", "Proveedor", "Última Venta", "Última Actualización" };
             foreach (string header in headers)
             {
-                PdfPCell cell = new PdfPCell(new Phrase(header, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12)));
+                PdfPCell cell = new PdfPCell(new Phrase(header, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)));
                 cell.BackgroundColor = BaseColor.LIGHT_GRAY;
                 cell.HorizontalAlignment = Element.ALIGN_CENTER;
                 tabla.AddCell(cell);
             }
 
-            
             foreach (var item in items)
             {
                 tabla.AddCell(item.ID.ToString());
                 tabla.AddCell(item.CodigoBusqueda);
                 tabla.AddCell(item.Descripcion);
-                tabla.AddCell(item.Precio.ToString("C"));
+                tabla.AddCell(item.Precio.ToString("N2")); 
                 tabla.AddCell(item.Cantidad.ToString());
+                tabla.AddCell(item.IDProveedor.ToString());
+                tabla.AddCell(item.UltimaVenta?.ToString("dd/MM/yyyy") ?? "N/A"); 
+                tabla.AddCell(item.UltimaActualizacion.ToString("dd/MM/yyyy"));
             }
 
             doc.Add(tabla);
