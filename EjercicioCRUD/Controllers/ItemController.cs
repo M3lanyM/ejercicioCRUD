2f56abf7f9f736f86e3a370e12a5f386a2c59f86
Melany Mendoza <melanymendoza2001@gmail.com>
2025-03-04 00:20:45 -0600

    exportPDF

--- EjercicioCRUD/Controllers/ItemController.cs
+++ EjercicioCRUD/Controllers/ItemController.cs
@@ -1,9 +1,13 @@
 ﻿using EjercicioCRUD.Models;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace EjercicioCRUD.Controllers
 {
@@ -63,6 +67,54 @@ namespace EjercicioCRUD.Controllers
         {
             _repository.EliminarItem(id);
             return RedirectToAction("Index");
+        }
+        public ActionResult ExportarPDF()
+        {
+            var items = _repository.ObtenerItems(1, int.MaxValue);
+
+            Document doc = new Document(PageSize.A4, 20, 20, 30, 50);
+            MemoryStream memoryStream = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(doc, memoryStream);
+            writer.PageEvent = new PDFNumPag(); 
+
+            doc.Open();
+
+            
+            Paragraph encabezado = new Paragraph("Reporte de Ítems", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16));
+            encabezado.Alignment = Element.ALIGN_CENTER;
+            doc.Add(encabezado);
+            doc.Add(new Paragraph($"Generado el: {DateTime.Now:dd/MM/yyyy HH:mm}\n\n"));
+
+            
+            PdfPTable tabla = new PdfPTable(5);
+            tabla.WidthPercentage = 100;
+            float[] columnWidths = { 10f, 30f, 20f, 20f, 20f };
+            tabla.SetWidths(columnWidths);
+
+            
+            string[] headers = { "ID", "Código", "Descripción", "Precio", "Cantidad" };
+            foreach (string header in headers)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(header, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12)));
+                cell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                tabla.AddCell(cell);
+            }
+
+            
+            foreach (var item in items)
+            {
+                tabla.AddCell(item.ID.ToString());
+                tabla.AddCell(item.CodigoBusqueda);
+                tabla.AddCell(item.Descripcion);
+                tabla.AddCell(item.Precio.ToString("C"));
+                tabla.AddCell(item.Cantidad.ToString());
+            }
+
+            doc.Add(tabla);
+            doc.Close();
+
+            return File(memoryStream.ToArray(), "application/pdf", "Reporte_Items.pdf");
         }
     }
 }
---
+++ EjercicioCRUD/Models/PDFNumPag.cs
@@ -0,0 +1,23 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+
+namespace EjercicioCRUD.Models
+{
+    public class PDFNumPag : PdfPageEventHelper
+    {
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            PdfPTable footer = new PdfPTable(1);
+            footer.TotalWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin;
+            footer.DefaultCell.Border = Rectangle.NO_BORDER;
+
+            PdfPCell cell = new PdfPCell(new Phrase("Página " + writer.PageNumber, new Font(Font.FontFamily.HELVETICA, 10)));
+            cell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            cell.Border = Rectangle.NO_BORDER;
+
+            footer.AddCell(cell);
+            footer.WriteSelectedRows(0, -1, document.LeftMargin, document.BottomMargin, writer.DirectContent);
+        }
+    }
+}
